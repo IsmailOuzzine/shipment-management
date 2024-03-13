cfce0346b13d24a393aa2a4dcfaf6f0e5148deeb
Said JOUHARI <jouharisaid41@gmail.com>
2024-03-13 19:01:57 +0100

    project setup

---
+++ backend/shipment-management/src/main/java/com/capgemini/shipment/ShipmentManagementApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.shipment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipmentManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShipmentManagementApplication.class, args);
+	}
+
+}
---
+++ backend/shipment-management/src/test/java/com/capgemini/shipment/ShipmentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.shipment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipmentManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
